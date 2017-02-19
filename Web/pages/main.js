5068eecf3b2b1c18972e25d421eb27b5ef6de330
peakcary <peakcary@163.com>
2017-02-19 21:55:52 +0800

    'init'

---
+++ Web/pages/main.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#btnConfirm").click(function () {
+        getData(1);
+    });
+});
+
+function getData(index) {
+    $.ajax({
+        type: "post",
+        url: "../Handler/DataHandler.ashx?t=getLeftTree",
+        data: "index=" + index,
+        complete: function () {
+        },
+        success: function (data) {
+            alert(data);
+        }
+    });
+}
